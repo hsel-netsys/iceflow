3f1a8c907fefa6c08fa08da60a9674c8038c5b58
Jan Romann <jan.romann@hs-emden-leer.de>
2023-08-15 16:16:12 +0200

    chore: run clang-format via CMake build targets

---
+++ cmake/formatting.cmake
@@ -0,0 +1,36 @@
+# Copyright 2023 The IceFlow Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# This file introduces custom `format` and `check-format` targets which simplify
+# the process of formatting the project's source code as well as verifying it.
+#
+# After running `cmake .`, you can automatically format the codebase using `make
+# format`. To only check whether the codebase is properly formatted, run `make
+# check-format` instead.
+
+set(FORMATTING_SOURCES apps/**/*.cpp tests/*.cpp include/**/*.hpp)
+
+add_custom_target(format)
+add_custom_command(
+  TARGET format
+  COMMAND clang-format -i ${FORMATTING_SOURCES}
+  COMMENT "Formatting codebase...")
+
+add_custom_target(check-format)
+add_custom_command(
+  TARGET check-format
+  COMMAND clang-format -i ${FORMATTING_SOURCES} --dry-run -Werror
+  COMMENT "Verifying formatting...")
