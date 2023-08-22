b2b2a06cb0b2a2aeed37a83be6ad406c1457122c
Jan Romann <jan.romann@hs-emden-leer.de>
2023-08-22 12:18:11 +0200

    style(constants): add missing namespace comment

--- include/iceflow/constants.hpp
+++ include/iceflow/constants.hpp
@@ -27,7 +27,7 @@ namespace constants {
  * Duration for which an IceFlow producer considers a data packet fresh.
  */
 const ndn::time::seconds PRODUCER_FRESHNESS_PERIOD = ndn::time::seconds(4);
-}
+} // namespace constants
 } // namespace iceflow
 
 #endif // ICEFLOW_CORE_CONSTANTS_HPP
