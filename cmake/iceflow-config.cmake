86dc8af10948dd9fba9f3a07d7c71a21c96f968e
Jan Romann <jan.romann@hs-emden-leer.de>
2023-12-12 11:33:56 +0100

    fixup! initial SVS implementation

--- cmake/iceflow-config.cmake
+++ cmake/iceflow-config.cmake
@@ -4,6 +4,7 @@ find_dependency(Boost REQUIRED)
 find_dependency(PkgConfig)
 pkg_check_modules(libndn-cxx REQUIRED)
 pkg_check_modules(PSYNC REQUIRED PSync)
+pkg_check_modules(SVS REQUIRED libndn-svs)
 
 get_filename_component(SELF_DIR ${CMAKE_CURRENT_LIST_DIR} PATH)
 
