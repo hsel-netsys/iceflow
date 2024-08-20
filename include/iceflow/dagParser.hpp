3d1cc703599b598b5dfe511826e033e8ac9e069e
rgias <rgias@technik-emden.de>
2024-08-20 11:44:26 +0200

    feat: add dagParser header file

---
+++ include/iceflow/dagParser.hpp
@@ -0,0 +1,54 @@
+#ifndef DAGPARSER_HPP
+#define DAGPARSER_HPP
+
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using json = nlohmann::json;
+
+// Structs for resources, container, scaling parameters, and communication task
+struct Resources {
+  uint32_t cpu;
+  uint32_t memory;
+};
+
+struct Container {
+  std::string image;
+  Resources resources;
+};
+
+struct ScalingParameters {
+  uint32_t taskComplexity;
+};
+
+struct CommunicationTask {
+  std::optional<uint32_t> payloadSize;
+};
+
+// Paramters of a node in the DAG
+struct Node {
+  std::string task;
+  std::string name;
+  std::string description;
+  std::string executor;
+  Container container;
+  ScalingParameters scalingParameters;
+  CommunicationTask communicationTask;
+  std::optional<std::vector<std::string>> downstream;
+};
+
+// Class  for parsing the DAG application
+class DAGApplication {
+public:
+  std::string applicationName;
+  std::vector<Node> nodes;
+
+  static DAGApplication parseFromFile(const std::string &filename);
+};
+
+#endif // DAGPARSER_HPP
