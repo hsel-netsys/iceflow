8b413318bf68bd9d480173e78a67100a3bf6b073
T M Rayhan Gias <49976934+tanim-ics@users.noreply.github.com>
2024-09-05 15:08:46 +0200

    feat: add first draft of a DAG of applications (#75)
    * feat: add dataflow graph of wordcount app
    
    first draft of a dataflow graph of an application e.g.
    "Wordcount".
    
    * Update application-DAG.yml
    
    * Update examples/wordCount/application-DAG.yml
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * Update examples/wordCount/application-DAG.yml
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * fix: change from yml to json format
    
    * Update examples/wordCount/application-DAG.json
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * fix: remove upstream field
    
    * feat: add initial CDDL grammar for application DAGs
    
    * feat: adjust wordcount application DAG to fit CDDL grammar
    
    * fixup! feat: add initial CDDL grammar for application DAGs
    
    Co-authored-by: Hugo Hakim Damer <hugo.damer@hs-emden-leer.de>
    
    * fixup! feat: add initial CDDL grammar for application DAGs
    
    * fixup! feat: add initial CDDL grammar for application DAGs
    
    * fixup! feat: adjust wordcount application DAG to fit CDDL grammar
    
    * fixup! feat: adjust wordcount application DAG to fit CDDL grammar
    
    * feat: add dagParser header file
    
    * fix! minnor typos
    
    * fix: add dagParser.cpp
    
    * fix: add dagParser.cpp
    
    * fix: removed TODO
    
    * fix: minor fix
    
    * fix: namespace issues
    
    * fix: add applicationName and nodes as constructor parameter
    
    * fix: minor fix in printNodeDetails method
    
    * fix! edge description
    
    * fix! add edge description
    
    * feat! finding node details as per Name
    
    * fix! minor formatting
    
    * fix! update dagParser.cpp
    
    * Update include/iceflow/dagParser.hpp
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * Update src/dagParser.cpp
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * Update src/dagParser.cpp
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * Update examples/wordCount/application-DAG.json
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * Update src/dagParser.cpp
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * Update include/iceflow/dagParser.hpp
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * Update include/iceflow/dagParser.hpp
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * Update include/iceflow/dagParser.hpp
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * Update src/dagParser.cpp
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * fix! remove printNodeDetails method
    
    * Update dag-syntax.cddl
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * Update examples/wordCount/application-DAG.json
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    
    * fix! update dag-parser
    
    * fix! Update CMakeLists.txt
    
    * fix! minor fix
    
    * refactor: refactor node and edge finding methods
    
    * fix: use the correct name for dag-parser.cpp
    
    * chore(dag-syntax.cddl): add copyright header
    
    * style(dag-syntax.cddl): remove trailing whitespace
    
    * fixup! fix: use the correct name for dag-parser.cpp
    
    * fix: adjust name of dag-Parser.hpp
    
    * feat: don't use optional for list of downstream edges
    
    ---------
    
    Co-authored-by: Jan Romann <jan.romann@hs-emden-leer.de>
    Co-authored-by: Hugo Hakim Damer <hugo.damer@hs-emden-leer.de>

---
+++ include/iceflow/dag-parser.hpp
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2024 The IceFlow Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef DAGPARSER_HPP
+#define DAGPARSER_HPP
+
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using json = nlohmann::json;
+
+namespace iceflow {
+
+// Structs for resources, container, scaling parameters, and communication task
+struct Resources {
+  uint32_t cpu;
+  uint32_t memory;
+};
+
+struct Container {
+  std::string image;
+  Resources resources;
+};
+
+struct ScalingParameters {
+  uint32_t taskComplexity;
+};
+
+struct CommunicationTask {
+  std::optional<uint32_t> payloadSize;
+};
+
+struct Edge {
+  std::string id;
+  std::string target;
+  uint32_t maxPartitions;
+};
+
+/**
+ * Parameters of a node in the DAG.
+ */
+struct Node {
+  std::string task;
+  std::string name;
+  std::string description;
+  std::string executor;
+  Container container;
+  ScalingParameters scalingParameters;
+  CommunicationTask communicationTask;
+  std::vector<Edge> downstream;
+};
+
+/**
+ * Class for parsing the application DAG.
+ */
+class DAGParser {
+public:
+  DAGParser(const std::string &appName, const std::vector<Node> &nodes);
+
+  ~DAGParser();
+
+  static DAGParser parseFromFile(const std::string &filename);
+
+  const std::vector<Node> &getNodes();
+
+  const Node &findNodeByName(const std::string &nodeName);
+
+  const Edge &findEdgeByName(const std::string &edgeId);
+
+  std::vector<std::pair<const Node &, const Edge &>>
+  findUpstreamEdges(const std::string &node_name);
+
+private:
+  std::string applicationName;
+  std::vector<Node> nodes;
+};
+
+} // namespace iceflow
+
+#endif // DAGPARSER_HPP
---
+++ src/dag-parser.cpp
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2024 The IceFlow Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "ndn-cxx/util/logger.hpp"
+
+#include "dag-parser.hpp"
+
+namespace iceflow {
+
+NDN_LOG_INIT(iceflow.DAGParser);
+
+DAGParser::DAGParser(const std::string &appName,
+                     const std::vector<Node> &nodeList)
+    : applicationName(appName), nodes(nodeList) {}
+
+DAGParser DAGParser::parseFromFile(const std::string &filename) {
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    throw std::runtime_error("File can not be opened " + filename);
+  }
+
+  json dagParam;
+  file >> dagParam;
+
+  std::string appName = dagParam.at("applicationName").get<std::string>();
+
+  std::vector<Node> nodeList;
+  for (const auto &nodeJson : dagParam.at("nodes")) {
+    Node nodeInstance;
+
+    // Tasks
+    nodeInstance.task = nodeJson.at("task").get<std::string>();
+    nodeInstance.name = nodeJson.at("name").get<std::string>();
+    nodeInstance.description = nodeJson.at("description").get<std::string>();
+    nodeInstance.executor = nodeJson.at("executor").get<std::string>();
+
+    // Container
+    nodeInstance.container.image =
+        nodeJson.at("container").at("image").get<std::string>();
+    nodeInstance.container.resources.cpu =
+        nodeJson.at("container").at("resources").at("cpu").get<uint32_t>();
+    nodeInstance.container.resources.memory =
+        nodeJson.at("container").at("resources").at("memory").get<uint32_t>();
+
+    // Scaling parameters
+    nodeInstance.scalingParameters.taskComplexity =
+        nodeJson.at("scalingParameters").at("taskComplexity").get<uint32_t>();
+
+    // Communication task with optional payload size
+    if (nodeJson.contains("communicationTask") &&
+        nodeJson["communicationTask"].contains("payloadSize")) {
+      nodeInstance.communicationTask.payloadSize =
+          nodeJson["communicationTask"]["payloadSize"].get<uint32_t>();
+    }
+
+    // downstream edges
+    if (nodeJson.contains("downstream")) {
+      std::vector<Edge> edges;
+      for (const auto &edgeJson : nodeJson.at("downstream")) {
+        Edge edgeInstance;
+        edgeInstance.id = edgeJson.at("id").get<std::string>();
+        edgeInstance.target = edgeJson.at("target").get<std::string>();
+        edgeInstance.maxPartitions =
+            edgeJson.at("maxPartitions").get<uint32_t>();
+        edges.push_back(edgeInstance);
+      }
+      nodeInstance.downstream = edges;
+    }
+
+    nodeList.push_back(nodeInstance);
+  }
+
+  return DAGParser(appName, nodeList);
+}
+
+const std::vector<Node> &DAGParser::getNodes() { return nodes; }
+
+const Node &DAGParser::findNodeByName(const std::string &nodeName) {
+  auto it =
+      std::find_if(nodes.begin(), nodes.end(), [&nodeName](const Node &node) {
+        return node.name == nodeName;
+      });
+
+  if (it != nodes.end()) {
+    return *it;
+  }
+
+  throw std::runtime_error("Node with name '" + nodeName + "' not found");
+}
+
+const Edge &DAGParser::findEdgeByName(const std::string &edgeId) {
+  for (const auto &node : nodes) {
+    auto downstream = node.downstream;
+
+    auto it =
+        std::find_if(downstream.begin(), downstream.end(),
+                     [&edgeId](const Edge &edge) { return edge.id == edgeId; });
+
+    if (it != downstream.end()) {
+      return *it;
+    }
+  }
+
+  throw std::runtime_error("Edge with ID '" + edgeId + "' not found");
+}
+
+std::vector<std::pair<const Node &, const Edge &>>
+DAGParser::findUpstreamEdges(const std::string &nodeName) {
+  std::vector<std::pair<const Node &, const Edge &>> upstreamEdges;
+
+  for (const auto &node : nodes) {
+    auto downstream = node.downstream;
+
+    auto it = std::find_if(
+        downstream.begin(), downstream.end(),
+        [&nodeName](const Edge &edge) { return edge.target == nodeName; });
+
+    if (it != downstream.end()) {
+      upstreamEdges.emplace_back(node, *it);
+    }
+  }
+
+  return upstreamEdges;
+}
+
+} // namespace iceflow
