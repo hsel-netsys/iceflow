774a3dd689d717f3ef51edc2f8a7fd6fa6126aea
rgias <rgias@technik-emden.de>
2024-09-11 16:54:11 +0200

    feat! add serialization & deserialization to/from CBOR

---
+++ include/iceflow/contentType.hpp
@@ -0,0 +1,10 @@
+#ifndef CONTENT_TYPE_HPP
+#define CONTENT_TYPE_HPP
+#include <cstdint>
+// ContentType header for defining custom TLV types
+
+namespace ContentType {
+const uint16_t JsonObject = 128; // nlohmann::json object
+}
+
+#endif // CONTENT_TYPE_HPP
